968af588e27c958ae842e2cf832f9295a2846453
Manu George <30804841+mgstrikes@users.noreply.github.com>
2019-03-12 09:15:27 +0530

    Initial check in

---
+++ src/app/app.component.ts
@@ -0,0 +1,131 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ModalDirective } from '../../node_modules/ngx-bootstrap';
+import { QuestionClass } from './question-class';
+import { ToastrService } from 'ngx-toastr';
+
+
+
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+	isQuestionCardShow: boolean = false;
+	totalAnswered: number = 0;
+	rightAnswer: number;
+	questionObj = new QuestionClass();
+	@ViewChild('submitModal') submitModal: ModalDirective;
+	@ViewChild('addQuestionModal') addQuestionModal : ModalDirective;
+	@ViewChild('answerModal') answerModal : ModalDirective;
+	@ViewChild('questionForm') questionForm: any;
+	@ViewChild('questionTest') questionTest : any;
+
+	constructor( private toastr: ToastrService) { }
+
+	answerArray = [];
+
+	allQuestions: any = [{
+		"id": 1,
+		"question": "Which is the largest country in the world by population?",
+		"a": "India",
+		"b": "USA",
+		"c": "China",
+		"d": "Russia",
+		"answer": "c"
+	},
+	{
+		"id": 2,
+		"question": "When did the second world war end?",
+		"a": "1945",
+		"b": "1939",
+		"c": "1944",
+		"d": "1942",
+		"answer": "a"
+	},
+	{
+		"id": 3,
+		"question": "Which was the first country to issue paper currency?",
+		"a": "USA",
+		"b": "France",
+		"c": "Italy",
+		"d": "China",
+		"answer": "d"
+	},
+	{
+		"id": 4,
+		"question": "Which city hosted the 1996 Summer Olympics?",
+		"a": "Altana",
+		"b": "Sydney",
+		"c": "Athens",
+		"d": "Beijing",
+		"answer": "a"
+	},
+	{
+		"id": 5,
+		"question": "Who invented telephone?",
+		"a": "Albert Einstein",
+		"b": "Alexander Graham Bell",
+		"c": "Isaac Newton",
+		"d": "Marie Curie",
+		"answer": "b"
+	}
+	];
+
+	/**Method call on submit the test */
+	submitTest() {
+		this.rightAnswer = 0;
+		this.totalAnswered = 0;
+		for (let i = 0; i < this.allQuestions.length; i++) {
+			if ("selected" in this.allQuestions[i] && (this.allQuestions[i]["selected"] != null)) {
+				this.totalAnswered++;
+				if (this.allQuestions[i]["selected"] == this.allQuestions[i]["answer"]) {
+					this.rightAnswer++;
+				}
+			}
+
+		}
+		this.submitModal.show();
+
+	}
+
+	startQuiz() {
+		for (let i = 0; i < this.allQuestions.length; i++) {
+			if ("selected" in this.allQuestions[i]) {
+				delete this.allQuestions[i]["selected"];
+			}
+
+		}
+		this.questionTest.reset();
+		this.isQuestionCardShow = true;
+
+	}
+	HomePage() {
+		this.isQuestionCardShow = false;
+	}
+	addQuestion(){
+		this.addQuestionModal.show();
+	}
+	submitAddQuestion(){
+		let quesTemp = JSON.parse(JSON.stringify(this.questionObj));
+		quesTemp["id"] = this.allQuestions.length+1;
+		this.allQuestions.push(quesTemp);
+		this.questionForm.reset();
+		this.toastr.success("Question Added Successfully!!");
+		this.addQuestionModal.hide();
+
+	}
+	checkAnswers(){
+		this.submitModal.hide();
+		this.answerModal.show();
+	}
+
+	ngOnInit() {
+
+
+
+	}
+
+}
